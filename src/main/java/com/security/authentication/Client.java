b0e5d8bc73feedf3675aea993d7a2136aaffbdb2
FredslundMagnus <s183905@student.dtu.dk>
2022-10-26 16:36:18 +0200

    fd

--- src/main/java/com/security/authentication/Client.java
+++ src/main/java/com/security/authentication/Client.java
@@ -11,6 +11,10 @@ import java.rmi.RemoteException;
 public class Client {
     public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
         PrintService service = (PrintService) Naming.lookup("rmi://localhost:5099/print");
+        System.out.println("Before");
         service.start();
+        service.setConfig("user", "Magnus");
+        System.out.println("After");
+        System.out.println(service.readConfig("user"));
     }
 }
--- src/main/java/com/security/authentication/PrintServant.java
+++ src/main/java/com/security/authentication/PrintServant.java
@@ -2,63 +2,67 @@ package com.security.authentication;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PrintServant extends UnicastRemoteObject implements PrintService {
+    private Map<String, String> config;
+    private HashMap<String, ArrayList<String>> queues;
+
     protected PrintServant() throws RemoteException {
         super();
+        this.config = new HashMap<String, String>();
+        this.queues = new HashMap<String, ArrayList<String>>();
     }
 
     @Override
     public void print(String filename, String printer) throws RemoteException {
-        // TODO Auto-generated method stub
-
+        queues.get(printer).add(filename);
     }
 
     @Override
-    public void queue(String printer) throws RemoteException {
-        // TODO Auto-generated method stub
-
+    public ArrayList<String> queue(String printer) throws RemoteException {
+        return queues.get(printer);
     }
 
     @Override
     public void topQueue(String printer, int job) throws RemoteException {
-        // TODO Auto-generated method stub
-
+        String old = queues.get(printer).remove(job);
+        queues.get(printer).add(0, old);
     }
 
     @Override
     public void start() throws RemoteException {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void stop() throws RemoteException {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public void restart() throws RemoteException {
-        // TODO Auto-generated method stub
-
+        // stops the print server, clears the print queue and starts the print server
+        // again
+        stop();
+        // clear query
+        start();
     }
 
     @Override
-    public void status(String printer) throws RemoteException {
-        // TODO Auto-generated method stub
-
+    public String status(String printer) throws RemoteException {
+        return queues.get(printer).size() == 0 ? "Ready" : "Printing";
     }
 
     @Override
-    public void readConfig(String parameter) throws RemoteException {
-        // TODO Auto-generated method stub
-
+    public String readConfig(String parameter) throws RemoteException {
+        return config.get(parameter);
     }
 
     @Override
     public void setConfig(String parameter, String value) throws RemoteException {
-        // TODO Auto-generated method stub
-
+        config.put(parameter, value);
     }
 }
