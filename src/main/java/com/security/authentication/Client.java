e39bd9aa7320e67a52ec2f187275fcf013639986
FredslundMagnus <s183905@student.dtu.dk>
2022-11-02 15:19:19 +0100

    d

--- src/main/java/com/security/authentication/Client.java
+++ src/main/java/com/security/authentication/Client.java
@@ -4,9 +4,6 @@ import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-/**
- * Hello world!
- */
 public class Client {
     public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
         ClientService client = new ClientService();
