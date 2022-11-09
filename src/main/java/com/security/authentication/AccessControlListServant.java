ef4d75c57ea085379e6972c872ef844f9bd7d970
FredslundMagnus <s183905@student.dtu.dk>
2022-11-09 17:20:21 +0100

    Implemented Access Control List

--- src/main/java/com/security/authentication/AccessControlListServant.java
+++ src/main/java/com/security/authentication/AccessControlListServant.java
@@ -1,6 +1,11 @@
 package com.security.authentication;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.rmi.RemoteException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class AccessControlListServant extends AbstractServant {
     protected AccessControlListServant(String configFile) throws RemoteException {
@@ -9,6 +14,25 @@ public class AccessControlListServant extends AbstractServant {
 
     @Override
     public boolean hasAccess(String user, String method) {
-        return true;
+        try {
+            File myObj = new File(configFile);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                if (line.startsWith(method + ",", 0)) {
+                    List<String> usersWithAccess = Arrays.asList(line.split(",")).subList(1,
+                            Arrays.asList(line.split(",")).size());
+                    if (usersWithAccess.contains(user)) {
+                        myReader.close();
+                        return true;
+                    }
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return false;
     }
 }
--- src/main/java/com/security/authentication/Client.java
+++ src/main/java/com/security/authentication/Client.java
@@ -8,9 +8,12 @@ public class Client {
     public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
         Tasks task = Tasks.AccessControlList;
         ClientService client = new ClientService(task);
-        // ClientService client2 = new ClientService(task);
+        ClientService client2 = new ClientService(task);
 
         client.login("Alice", "AlicePassword");
+        client.print("ssdf", "printer 1");
+        client2.login("Bob", "BobPassword");
+        client2.print("ssdf", "printer 1");
 
     }
 }
